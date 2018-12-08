177de01787acd9479d441cd3316d694604debd17
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-12-07 23:31:47 -0600

    Complete initial UI autolayout for first screen.

--- Programmatic Autolayout/ViewController.swift
+++ Programmatic Autolayout/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK:- Properties
+    let iconImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "Cactus"))
+         imageView.translatesAutoresizingMaskIntoConstraints = false //enables autolayout for imageView
+        return imageView
+    }()
+    
+    let descriptionTextView: UITextView = {
+      let textView = UITextView()
+        textView.text = "Welcome to Aspira! Let's walk through the onboarding process - don't worry, it'll be brief!"
+        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        textView.textAlignment = .center
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.isEditable = false //no keyboard
+        textView.isScrollEnabled = false
+        return textView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(iconImageView)
+        view.addSubview(descriptionTextView)
+        
+        setupLayout()
+        
+    }
+    
+    private func setupLayout() {
+        //MARK:- Building Anchor constraints
+        //placed in the center X, 100 from the top in Y axis
+        let iconConstraints = [iconImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor), iconImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100), iconImageView.widthAnchor.constraint(equalToConstant: 200), iconImageView.heightAnchor.constraint(equalToConstant: 200)]
+        
+        let textViewConstraints = [descriptionTextView.topAnchor.constraint(equalTo: iconImageView.bottomAnchor, constant: 120), descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor), descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor), descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor)]
+        
+        NSLayoutConstraint.activate(iconConstraints)
+        NSLayoutConstraint.activate(textViewConstraints)
     }
 
 
