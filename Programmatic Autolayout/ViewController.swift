c4ed7b4e6cd860839b1369aefbc11cd957ce8c64
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-12-07 22:19:51 -0600

    Initial Commit

---
+++ Programmatic Autolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Programmatic Autolayout
+//
+//  Created by Charles Martin Reed on 12/7/18.
+//  Copyright © 2018 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
