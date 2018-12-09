6e3d730f4d14dd85999620b1413332be978137fa
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-12-08 19:54:21 -0600

    Add container for image view, add auto layout multipliers for positioning image within view.

--- Programmatic Autolayout/ViewController.swift
+++ Programmatic Autolayout/ViewController.swift
@@ -13,14 +13,18 @@ class ViewController: UIViewController {
     //MARK:- Properties
     let iconImageView: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "Cactus"))
-         imageView.translatesAutoresizingMaskIntoConstraints = false //enables autolayout for imageView
+        imageView.translatesAutoresizingMaskIntoConstraints = false //enables autolayout for imageView
+        imageView.contentMode = .scaleAspectFit //w and h stay the same regardless of imageView size
         return imageView
     }()
     
     let descriptionTextView: UITextView = {
       let textView = UITextView()
-        textView.text = "Welcome to Aspira! Let's walk through the onboarding process - don't worry, it'll be brief!"
-        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        let attributedText = NSMutableAttributedString(string: "Welcome to Aspira! Let's walk through the onboarding process - don't worry, it'll be brief!", attributes: [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 18)])
+        
+        attributedText.append(NSAttributedString(string: "\n\n\nAre you ready? You'll need access to your assigned work computer and, if applicable, your assigned iPhone. If you have not yet received those, please contact your branch IT department for assistance.", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 13), NSAttributedString.Key.foregroundColor : UIColor.gray]))
+        
+        textView.attributedText = attributedText
         textView.textAlignment = .center
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.isEditable = false //no keyboard
@@ -30,7 +34,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(iconImageView)
         view.addSubview(descriptionTextView)
         
         setupLayout()
@@ -38,16 +41,38 @@ class ViewController: UIViewController {
     }
     
     private func setupLayout() {
+        //MARK:- Container and constraints for container
+        let topImageContainerView = UIView()
+        view.addSubview(topImageContainerView)
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+       
         //MARK:- Building Anchor constraints
-        //placed in the center X, 100 from the top in Y axis
-        let iconConstraints = [iconImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor), iconImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100), iconImageView.widthAnchor.constraint(equalToConstant: 200), iconImageView.heightAnchor.constraint(equalToConstant: 200)]
+        //multiplier on heightAnchor means 1/2 of the parent view size - we also get landscape orientation handling with this sort of programmatic layout work
+        //leading and trailing over left and right because certain languages are read right-to-left. Leading and trailing handle this use case.
+        let topImageContainerConstraints = [
+            topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor),
+            topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5)]
         
-        let textViewConstraints = [descriptionTextView.topAnchor.constraint(equalTo: iconImageView.bottomAnchor, constant: 120), descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor), descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor), descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor)]
+        topImageContainerView.addSubview(iconImageView)
         
+        //iconImageView is now contained within our topImageContainerView
+        let iconConstraints = [
+            iconImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor),
+            iconImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor),
+            iconImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5)]
+        
+        let textViewConstraints = [
+            descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor),
+            descriptionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            descriptionTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor)]
+        
+        NSLayoutConstraint.activate(topImageContainerConstraints)
         NSLayoutConstraint.activate(iconConstraints)
         NSLayoutConstraint.activate(textViewConstraints)
     }
 
 
 }
-
