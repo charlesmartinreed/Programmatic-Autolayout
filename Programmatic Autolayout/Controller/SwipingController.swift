366b72050eb933815be4730d997140f1de48f155
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-12-09 18:39:59 -0600

    Fix scroll view issues in landscape mode.

--- Programmatic Autolayout/Controller/SwipingController.swift
+++ Programmatic Autolayout/Controller/SwipingController.swift
@@ -164,4 +164,25 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
         return CGSize(width: view.frame.width, height: view.frame.height)
     }
     
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        
+        //coordinator will run an animation during the update
+        coordinator.animate(alongsideTransition: { (_) in
+            self.collectionViewLayout.invalidateLayout() //lets the layout redraw itself during the next view layout update cycle
+            
+            //iPhone X/XS screen fix for page 0 layout issue
+            if self.pageControl.currentPage == 0 {
+                self.collectionView.contentOffset = .zero
+            } else {
+                //animate back to the proper page
+                let indexPath = IndexPath(item: self.pageControl.currentPage, section: 0)
+                self.collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            }
+        }) { (_) in
+            //do stuff
+        }
+        
+        
+    }
+    
 }
